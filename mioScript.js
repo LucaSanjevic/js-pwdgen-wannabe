bedefb16e34fa6e93b7c2d672322eae026b73397
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-04-08 15:57:27 +0200

    Corretto

---
+++ mioScript.js
@@ -0,0 +1,4 @@
+var nome = prompt('Qual è il tuo nome?');
+        var cognome = prompt('Qual è il tuo cognome?');
+        var colore = prompt('Qual è il tuo colore preferito?');
+        document.getElementById("stringa").innerHTML =nome + cognome + colore + "21";
